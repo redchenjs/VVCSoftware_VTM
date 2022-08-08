7d5b7275df91f3a494cfd9982c349b675221d2b3
Jack Chen <redchenjs@live.com>
2022-08-08 13:15:25 +0800

    amcp: add test equation solver

--- source/Lib/EncoderLib/InterSearch.cpp
+++ source/Lib/EncoderLib/InterSearch.cpp
@@ -8274,6 +8274,348 @@ void InterSearch::xCheckBestAffineMVP( PredictionUnit &pu, AffineAMVPInfo &affin
   }
 }
 
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+#define LOG_COLOR_BLACK   "30"
+#define LOG_COLOR_RED     "31"
+#define LOG_COLOR_GREEN   "32"
+#define LOG_COLOR_BROWN   "33"
+#define LOG_COLOR_BLUE    "34"
+#define LOG_COLOR_PURPLE  "35"
+#define LOG_COLOR_CYAN    "36"
+#define LOG_COLOR(COLOR)  "\033[0;" COLOR "m"
+#define LOG_BOLD(COLOR)   "\033[1;" COLOR "m"
+#define LOG_RESET_COLOR   "\033[0m"
+#define LOG_COLOR_E       LOG_COLOR(LOG_COLOR_RED)
+#define LOG_COLOR_W       LOG_COLOR(LOG_COLOR_BROWN)
+#define LOG_COLOR_I       LOG_COLOR(LOG_COLOR_GREEN)
+
+/*
+ 2  -3   1   5   6
+-3   1   2  -4   5
+-1  -2   3   1  11
+ 0   0   1   1   2
+
+ x0 = -3
+ x1 = -2
+ x2 =  1
+ x3 =  1
+*/
+
+/*
+ 0   1   2   2
+ 1   1   1   1
+ 0  -1  -4  -2
+ 3   2   1  -1
+
+ x0 = -2
+ x1 = -3
+ x2 = -1
+ x3 =  0
+*/
+
+double data[7][7] = {
+    [0] = { 0,   1,   2,   2,   1,   0,   0},
+    [1] = { 1,   1,   1,   1,   0,   0,   0},
+    [2] = { 0,  -1,  -4,  -2,   1,   0,   0},
+    [3] = { 3,   2,   1,  -1,  -1,   0,   0},
+    [4] = { 0,   0,   0,   0,   0,   0,   0},
+    [5] = { 0,   0,   0,   0,   0,   0,   0},
+    [6] = { 0,   0,   0,   0,   0,   0,   0},
+};
+
+void print_mat(const char *str, bool param6, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+
+    printf("------------------------------- %-4s ------------------------------ n = %d\n", str, n);
+    for (int p = 0; p < n; p++) {
+        printf(LOG_COLOR_W "r%d ", p);
+        printf(LOG_RESET_COLOR "|");
+        for (int q = 0; q < n; q++) {
+            printf(LOG_COLOR_I "%8.1f\t", C[p][q]);
+        }
+        printf(LOG_RESET_COLOR "|");
+        printf(LOG_COLOR_W "%8.1f", C[p][n]);
+        printf(LOG_RESET_COLOR "\n");
+    }
+}
+
+void print_mat(char idx, int k, bool param6, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+
+    printf("------------------------------- MATRIX %c -------------------- n = %d k = %d\n", idx, n, k);
+    for (int p = 0; p < n; p++) {
+        printf(LOG_COLOR_W "r%d ", p);
+        printf(LOG_RESET_COLOR "|");
+        for (int q = 0; q < n; q++) {
+            printf(LOG_COLOR_I "%8.1f\t", C[p][q]);
+        }
+        printf(LOG_RESET_COLOR "|");
+        printf(LOG_COLOR_W "%8.1f", C[p][n]);
+        printf(LOG_RESET_COLOR "\n");
+    }
+}
+
+void print_mat(char idx, int k, int m, bool param6, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+
+    printf("------------------------------- MATRIX %c -------------- n = %d k = %d m = %d\n", idx, n, k, m);
+    for (int p = 0; p < n; p++) {
+        printf(LOG_COLOR_W "r%d ", p);
+        printf(LOG_RESET_COLOR "|");
+        for (int q = 0; q < n; q++) {
+            printf(LOG_COLOR_E "%8.1f\t", C[p][q]);
+        }
+        printf(LOG_RESET_COLOR "|");
+        printf(LOG_COLOR_W "%8.1f", C[p][n]);
+        printf(LOG_RESET_COLOR "\n");
+    }
+}
+
+void print_res(bool param6, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+
+    printf("------------------------------- RESULT ----------------------------------\n");
+    for (int i = 0; i < n; i++) {
+        printf(LOG_COLOR_I "x%d = %8.1f", i, C[i][n] / C[i][i]);
+        printf(LOG_RESET_COLOR "\n");
+    }
+}
+
+void print_res(int a, bool param6, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+
+    printf("------------------------------- RESULT ----------------------------------\n");
+    for (int i = 0; i < n; i++) {
+        printf(LOG_COLOR_I "x%d = %8.1f", i, C[i][n] / C[i][i]);
+        printf(LOG_RESET_COLOR "\n");
+    }
+}
+
+void scale_mat(int b, bool param6, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+
+    for (int p = 0; p < n; p++) {
+        for (int q = 0; q < n; q++) {
+            if (b >= 0) {
+                C[p][q] = (int64_t)C[p][q] << abs(b);
+            } else {
+                C[p][q] = (int64_t)C[p][q] >> abs(b);
+            }
+        }
+    }
+}
+
+void method_gja(bool param6, bool pivoting, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+    double D[7][7] = { 0 };
+
+    print_mat("GJA", param6, C);
+
+    for (int k = 0; k < n; k++) {
+        if (pivoting) {
+            // find column max
+            int m = k;
+            double t = fabs(C[k][k]);
+
+            for (int i = k + 1; i < n; i++) {
+                if (fabs(C[i][k]) > t) {
+                    t = fabs(C[i][k]);
+                    m = i;
+                }
+            }
+
+            // swap rows k and m
+            if (m != k) {
+                print_mat('A', k, m, param6, C);
+
+                for (int j = 0; j < n + 1; j++) {
+                    C[6][j] = C[k][j];
+                    C[k][j] = C[m][j];
+                    C[m][j] = C[6][j];
+                }
+
+                print_mat('B', k, m, param6, C);
+            }
+        }
+
+        double M = C[k][k];
+        for (int i = 0; i < n; i++) {
+            double L = C[i][k];
+
+            if (k == i) {
+                // make C[i][k] one
+                for (int j = 0; j < n + 1; j++) {
+                    D[k][j] = C[k][j] / M;
+                }
+            } else {
+                // make C[i][k] zero
+                for (int j = 0; j < n + 1; j++) {
+                    D[i][j] = C[i][j] - (L / M) * C[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                C[i][j] = D[i][j];
+            }
+        }
+
+        print_mat('C', k, param6, C);
+    }
+
+    print_res(param6, C);
+}
+
+void method_dfa(bool param6, bool pivoting, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+    int64_t D[7][7] = { 0 };
+
+    print_mat("DFA", param6, C);
+
+    for (int k = 0; k < n; k++) {
+        if (pivoting) {
+            // find column max
+            int m = k;
+            int t = fabs(C[k][k]);
+
+            for (int i = k + 1; i < n; i++) {
+                if (fabs(C[i][k]) > t) {
+                    t = fabs(C[i][k]);
+                    m = i;
+                }
+            }
+
+            // swap rows k and m
+            if (m != k) {
+                print_mat('A', k, m, param6, C);
+
+                for (int j = 0; j < n + 1; j++) {
+                    C[6][j] = C[k][j];
+                    C[k][j] = C[m][j];
+                    C[m][j] = C[6][j];
+                }
+
+                print_mat('B', k, m, param6, C);
+            }
+        }
+
+        int M = C[k][k];
+        for (int i = 0; i < n; i++) {
+            int L = C[i][k];
+
+            if (k == i) {
+                // row k is not modified
+                for (int j = 0; j < n + 1; j++) {
+                    D[k][j] = C[k][j];
+                }
+            } else {
+                // make C[i][k] zero
+                for (int j = 0; j < n + 1; j++) {
+                    D[i][j] = M * C[i][j] - L * C[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                C[i][j] = D[i][j];
+            }
+        }
+
+        print_mat('C', k, param6, C);
+    }
+
+    print_res(param6, C);
+}
+
+void method_dfa2(bool param6, bool pivoting, double C[7][7])
+{
+    int n = param6 ? 6 : 4;
+    int64_t D[7][7] = { 0 };
+
+    scale_mat(8, param6, C);
+    print_mat("DFA2", param6, C);
+
+    for (int k = 0; k < n; k++) {
+        if (pivoting) {
+            // find column max
+            int m = k;
+            int t = fabs(C[k][k]);
+
+            for (int i = k + 1; i < n; i++) {
+                if (fabs(C[i][k]) > t) {
+                    t = fabs(C[i][k]);
+                    m = i;
+                }
+            }
+
+            // swap rows k and m
+            if (m != k) {
+                print_mat('A', k, m, param6, C);
+
+                for (int j = 0; j < n + 1; j++) {
+                    C[6][j] = C[k][j];
+                    C[k][j] = C[m][j];
+                    C[m][j] = C[6][j];
+                }
+
+                print_mat('B', k, m, param6, C);
+            }
+        }
+
+        int M = C[k][k];
+        int B = 0;
+        int TM = abs(M);
+
+        while (TM >>= 1) {
+            B++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            int L = C[i][k];
+
+            if (k == i) {
+                // row k is not modified
+                for (int j = 0; j < n + 1; j++) {
+                    D[k][j] = C[k][j];
+                }
+            } else {
+                // make C[i][k] zero
+                for (int j = 0; j < n + 1; j++) {
+                    D[i][j] = M * C[i][j] - L * C[k][j];
+                    D[i][j] = D[i][j] >> B;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 2; j++) {
+                C[i][j] = D[i][j];
+            }
+        }
+
+        print_mat('C', k, param6, C);
+    }
+
+    scale_mat(-8, param6, C);
+    print_res(0, param6, C);
+}
+
 #if GDR_ENABLED
 void InterSearch::xAffineMotionEstimation(PredictionUnit &pu, PelUnitBuf &origBuf, RefPicList eRefPicList,
                                           Mv acMvPred[3], int refIdxPred, Mv acMv[3], bool acMvSolid[3],
@@ -8510,7 +8852,30 @@ void InterSearch::xAffineMotionEstimation(PredictionUnit &pu, PelUnitBuf &origBu
     double dDeltaMv[6]={0.0, 0.0, 0.0, 0.0, 0.0, 0.0,};
     Mv acDeltaMv[3];
 
+    for ( int row = 0; row < iParaNum; row++ )
+    {
+      for ( int i = 0; i < iParaNum; i++ )
+      {
+        data[row][i] = (double)i64EqualCoeff[row+1][i];
+      }
+    }
+
     solveEqual( pdEqualCoeff, affineParaNum, dAffinePara );
+
+    for (int i = 0; i < affineParaNum; i++) {
+      printf("x%d = %8.1f\n", dAffinePara[i]);
+    }
+
+    double C[7][7] = {0};
+
+    memcpy(C, data, sizeof(C));
+    method_gja(false, true, C);
+
+    memcpy(C, data, sizeof(C));
+    method_dfa(false, true, C);
+
+    memcpy(C, data, sizeof(C));
+    method_dfa2(false, true, C);
 
     // convert to delta mv
     dDeltaMv[0] = dAffinePara[0];
