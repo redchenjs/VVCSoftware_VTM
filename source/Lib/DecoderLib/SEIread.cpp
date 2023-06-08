32a53648e12a8383419c21a05aebb0ea68f2f724
林超逸 <linchaoyi.cy@bytedance.com>
2023-06-08 21:14:04 +0800

    JVET-AD0091: fix the compile bug

--- source/Lib/DecoderLib/SEIread.cpp
+++ source/Lib/DecoderLib/SEIread.cpp
@@ -2925,14 +2925,18 @@ void SEIReader::xParseSEINNPostFilterCharacteristics(SEINeuralNetworkPostFilterC
       sei.m_picHeightNumeratorMinus1 = val;
       sei_read_uvlc(pDecodedMessageOutputStream, val, "nnpfc_pic_height_denominator_minus1");
       sei.m_picHeightDenominatorMinus1 = val;
+#if JVET_AD0091
+      int heightRatio = (sei.m_picHeightNumeratorMinus1 + 1) / (sei.m_picHeightDenominatorMinus1 + 1);
+      int widthRatio = (sei.m_picWidthNumeratorMinus1 + 1) / (sei.m_picWidthDenominatorMinus1 + 1);
+
+      CHECK((heightRatio >= 1) && (heightRatio <= 16), "The value range of heightRatio shall be in the range of 1 to 16, inclusive");
+      CHECK((widthRatio >= 1) && (widthRatio <= 16), "The value range of heightRatio shall be in the range of 1 to 16, inclusive");
+#endif
 #else
       sei_read_flag(pDecodedMessageOutputStream, val, "nnpfc_pic_width_in_luma_samples");
       sei.m_picWidthInLumaSamples = val;
       sei_read_flag(pDecodedMessageOutputStream, val, "nnpfc_pic_height_in_luma_samples");
       sei.m_picHeightInLumaSamples = val;
-#if JVET_AD0091
-      CHECK(!(sei->m_picWidthInLumaSamples >= croppedWidth && sei->m_picWidthInLumaSamples <= croppedWidth * 16), "m_picWidthInLumaSamples shall be in the range of croppedWidth to croppedWidth * 16");
-      CHECK(!(sei->m_picHeightInLumaSamples >= croppedHeight && sei->m_picHeightInLumaSamples <= croppedHeight * 16), "m_picHeightInLumaSamples shall be in the range of croppedHeight to croppedHeight * 16");
 #endif
     }
 
@@ -3127,7 +3131,7 @@ void SEIReader::xParseSEINNPostFilterCharacteristics(SEINeuralNetworkPostFilterC
     sei_read_uvlc(pDecodedMessageOutputStream, val, "nnpfc_padding_type");
     sei.m_paddingType = val;
 #if JVET_AD0091
-  CHECK(sei.m_paddingType >= 5 && sei.m_targetId <= 15, "Reserved nnpfc_padding_type value, shall ignore the SEI message");
+  CHECK((sei.m_paddingType >= 5) && (sei.m_paddingType <= 15), "Reserved nnpfc_padding_type value, shall ignore the SEI message");
 #endif
 
     if (sei.m_paddingType == NNPC_PaddingType::FIXED_PADDING)
