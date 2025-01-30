e7c919691c79b05ac5645bad764518c6352e2039
Kenneth Andersson <kenneth.r.andersson@ericsson.com>
2025-01-30 08:48:16 +0100

    JVET-AK0181: Fix to remove enabling of the DMVR encoder control when GOPbasedRPR is enabled

--- source/App/EncoderApp/EncAppCfg.cpp
+++ source/App/EncoderApp/EncAppCfg.cpp
@@ -2245,8 +2245,6 @@ bool EncAppCfg::parseCfg( int argc, char* argv[] )
       m_scalingRatioHor = 2.0;
       m_scalingRatioVer = 2.0;
     }
-    // enable dmvr encoder selection
-    m_dmvrEncSelect = true;
   }
   m_resChangeInClvsEnabled = m_scalingRatioHor != 1.0 || m_scalingRatioVer != 1.0 || m_gopBasedRPREnabledFlag || m_rprFunctionalityTestingEnabledFlag;
   m_resChangeInClvsEnabled = m_resChangeInClvsEnabled && m_rprEnabledFlag;
